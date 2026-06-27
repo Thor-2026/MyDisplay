4c8f7386459bd60cb858da52444d61682c6c5395
Thor-2026 <strange98723@gmail.com>
2026-06-27 12:45:21 -0700

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,1 +1,24 @@
+const SUPABASE_URL = "https://ggohaurqbsyamznzuuaf.supabase.co";
 
+const SUPABASE_KEY = "sb_publishable_ZpHvrV-a_BmznhfbVupvxQ_b5NKYPYk";
+
+const supabaseClient = supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
+
+async function loadSettings() {
+
+    const { data, error } = await supabaseClient
+        .from("settings")
+        .select("*")
+        .eq("id", 1)
+        .single();
+
+    if (error) {
+        console.error(error);
+        return null;
+    }
+
+    return data;
+}
