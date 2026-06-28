4dcff76ef88a2b4c5ffedcb38c93d41abb753a76
Thor-2026 <strange98723@gmail.com>
2026-06-27 21:30:08 -0700

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,8 +1,15 @@
-const SUPABASE_URL = "https://ggohaurqbsyamznzuuaf.supabase.co";
+/*const SUPABASE_URL = "https://ggohaurqbsyamznzuuaf.supabase.co";
 
 const SUPABASE_KEY = "sb_publishable_ZpHvrV-a_BmznhfbVupvxQ_b5NKYPYk";
 
 const supabase = supabaseClient.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);*/
+const SUPABASE_URL = "https://ggohaurqbsyamznzuuaf.supabase.co";
+const SUPABASE_KEY = "sb_publishable_ZpHvrV-a_BmznhfbVupvxQ_b5NKYPYk";
+
+const supabaseClient = window.supabase.createClient(
     SUPABASE_URL,
     SUPABASE_KEY
 );
