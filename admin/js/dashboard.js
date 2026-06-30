fc6ec93d0bbba1632e2a95704ad956160473be0f
Thor-2026 <strange98723@gmail.com>
2026-06-29 17:44:19 -0700

    Update dashboard.js

--- admin/js/dashboard.js
+++ admin/js/dashboard.js
@@ -1,1 +1,12 @@
+document.getElementById("logoutBtn").addEventListener("click", async () => {
 
+    const { error } = await supabaseClient.auth.signOut();
+
+    if (error) {
+        console.error(error);
+        return;
+    }
+
+    window.location.href = "index.html";
+
+});
